249ef59d90e5444fb1cbfef839a530e4361cf74f
Hacke025 <rushidivy@gmail.com>
2025-03-24 21:09:00 -0400

    FINAL TOUCHES

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@onready var game_manager = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(_body):
+	game_manager.add_point()
 	print("+1 coin!")
-	queue_free()
+	animation_player.play("pickup")
